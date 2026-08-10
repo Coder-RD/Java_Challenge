332a6277970be57941ae028401c1a75358e21182
Coder-RD <rohinidatal0@gmail.com>
2026-08-10 21:28:42 +0530

    Java_Challenge-Day15
    ☕ Day 15/100 – Java Substring 🚀
    
    🎯 100 Days of Java Challenge
    
    Successfully completed Day 15 of my Java learning journey! 💻🔥
    
    Today’s HackerRank challenge was Java Substring, where I practiced extracting a specific portion of a string using Java’s substring() method.
    
    📚 Concepts Practiced
    🔤 String Manipulation
    ✂️ substring() Method
    📍 String Indexing
    🧠 Logical Thinking
    💻 Problem Solving
    
    Day 15/100 Completed 🎉
    🔥 15 Days Down — 85 More to Go!
    
    🚀 Learn. Practice. Code. Repeat.
    Every day brings me one step closer to becoming a better Java developer.
    
    #100DaysOfCode #Java #CoreJava #HackerRank #JavaDeveloper #CodingChallenge #GitHub #LearningInPublic #Programming #ProblemSolving #CodingJourney #DeveloperJourney

---
+++ Day-15/Java Substring.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        
+        System.out.println(s.substring(start,end));
+        sc.close();
+    }
+}
